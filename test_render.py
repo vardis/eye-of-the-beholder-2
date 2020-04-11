3c4f7141d16e8f3b14cf54848793ddaed42782b3
Giorgos Giannoudovardis <vardis.g@gmail.com>
2020-04-11 12:44:48 +0100

    Adds basic movement.

--- test_render.py
+++ test_render.py
@@ -63,6 +63,25 @@ class Direction(Enum):
     def from_view_dir(self, view_dir):
         return Direction.from_encoding((self.enc + view_dir.enc) & 0x03)  # mod 4
 
+    def opposite(self):
+        return Direction.from_encoding((self.enc + 2) & 0x3)
+
+    def update_pos(self, current_pos):
+        if self.enc == 0:
+            return current_pos[0], current_pos[1] - 1
+        elif self.enc == 1:
+            return current_pos[0] + 1, current_pos[1]
+        elif self.enc == 2:
+            return current_pos[0], current_pos[1] + 1
+        elif self.enc == 3:
+            return current_pos[0] - 1, current_pos[1]
+
+    def previous_dir(self):
+        return Direction.from_encoding((self.enc - 1) & 0x3)
+
+    def next_dir(self):
+        return Direction.from_encoding((self.enc + 1) & 0x3)
+
 
 class MazePosOffset:
     def __init__(self, dx, dy, direction):
@@ -73,49 +92,51 @@ class MazePosOffset:
 
 # @formatter:off
 maze_pos_offsets = [
-    MazePosOffset(-3, -3, Direction.East),   # A - east
-    MazePosOffset(-2, -3, Direction.East),   # B - east
-    MazePosOffset(-1, -3, Direction.East),   # C - east
+    MazePosOffset(-3, -3, Direction.East),  # A - east
+    MazePosOffset(-2, -3, Direction.East),  # B - east
+    MazePosOffset(-1, -3, Direction.East),  # C - east
 
-    MazePosOffset(1, -3, Direction.West),    # E - west
-    MazePosOffset(2, -3, Direction.West),    # F - west
-    MazePosOffset(3, -3, Direction.West),    # G - west
+    MazePosOffset(1, -3, Direction.West),  # E - west
+    MazePosOffset(2, -3, Direction.West),  # F - west
+    MazePosOffset(3, -3, Direction.West),  # G - west
 
     MazePosOffset(-2, -3, Direction.South),  # B - south
     MazePosOffset(-1, -3, Direction.South),  # C - south
-    MazePosOffset(0, -3, Direction.South),   # D - south
-    MazePosOffset(1, -3, Direction.South),   # E - south
-    MazePosOffset(2, -3, Direction.South),   # F - south
+    MazePosOffset(0, -3, Direction.South),  # D - south
+    MazePosOffset(1, -3, Direction.South),  # E - south
+    MazePosOffset(2, -3, Direction.South),  # F - south
 
-    MazePosOffset(-2, -2, Direction.East),   # H - east
-    MazePosOffset(-1, -2, Direction.East),   # I - east
+    MazePosOffset(-2, -2, Direction.East),  # H - east
+    MazePosOffset(-1, -2, Direction.East),  # I - east
 
-    MazePosOffset(1, -2, Direction.West),    # K - west
-    MazePosOffset(2, -2, Direction.West),    # L - west
+    MazePosOffset(1, -2, Direction.West),  # K - west
+    MazePosOffset(2, -2, Direction.West),  # L - west
 
     MazePosOffset(-1, -2, Direction.South),  # I - south
-    MazePosOffset(0, -2, Direction.South),   # J - south
-    MazePosOffset(1, -2, Direction.South),   # K - south
+    MazePosOffset(0, -2, Direction.South),  # J - south
+    MazePosOffset(1, -2, Direction.South),  # K - south
 
-    MazePosOffset(-1, -1, Direction.East),   # M - east
+    MazePosOffset(-1, -1, Direction.East),  # M - east
 
-    MazePosOffset(1, -1, Direction.West),    # O - west
+    MazePosOffset(1, -1, Direction.West),  # O - west
 
     MazePosOffset(-1, -1, Direction.South),  # M - south
-    MazePosOffset(1, -1, Direction.South),   # O - south
-    MazePosOffset(0, -1, Direction.South),   # N - south
+    MazePosOffset(1, -1, Direction.South),  # O - south
+    MazePosOffset(0, -1, Direction.South),  # N - south
 
-    MazePosOffset(-1, 0, Direction.East),    # P - east
+    MazePosOffset(-1, 0, Direction.East),  # P - east
 
-    MazePosOffset(1, 0, Direction.West)      # Q - west
+    MazePosOffset(1, 0, Direction.West)  # Q - west
 ]
+
+
 # @formatter:on
 
 
 class WallRenderConfig:
     def __init__(self, base_offset, view_offset, blk_height, blk_width, skip, flip):
         # offset within the tiles array
-        self.base_offset = base_offset
+        self.base_offset = base_offset + 101
 
         # block offset within the viewport
         self.view_offset = view_offset
@@ -132,41 +153,41 @@ class WallRenderConfig:
 
 
 walls_render_config = [
-    WallRenderConfig(3, 66, 5, 1, 2, False),      # A - east
-    WallRenderConfig(1, 68, 5, 3, 0, False),      # B - east
-    WallRenderConfig(-4, 74, 5, 1, 0, False),     # C - east
+    WallRenderConfig(3, 66, 5, 1, 2, False),  # A - east
+    WallRenderConfig(1, 68, 5, 3, 0, False),  # B - east
+    WallRenderConfig(-4, 74, 5, 1, 0, False),  # C - east
 
-    WallRenderConfig(-4, 79, 5, 1, 0, True),      # E - west
-    WallRenderConfig(1, 83, 5, 3, 0, True),       # F - west
-    WallRenderConfig(1, 87, 5, 1, 2, True),       # G - west
+    WallRenderConfig(-4, 79, 5, 1, 0, True),  # E - west
+    WallRenderConfig(1, 83, 5, 3, 0, True),  # F - west
+    WallRenderConfig(1, 87, 5, 1, 2, True),  # G - west
 
-    WallRenderConfig(32, 66, 5, 2, 4, False),     # B - south
-    WallRenderConfig(28, 68, 5, 6, 0, False),     # C - south
-    WallRenderConfig(28, 74, 5, 6, 0, False),     # D - south
-    WallRenderConfig(28, 80, 5, 6, 0, False),     # E - south
-    WallRenderConfig(28, 86, 5, 2, 4, False),     # F - south
+    WallRenderConfig(32, 66, 5, 2, 4, False),  # B - south
+    WallRenderConfig(28, 68, 5, 6, 0, False),  # C - south
+    WallRenderConfig(28, 74, 5, 6, 0, False),  # D - south
+    WallRenderConfig(28, 80, 5, 6, 0, False),  # E - south
+    WallRenderConfig(28, 86, 5, 2, 4, False),  # F - south
 
-    WallRenderConfig(16, 66, 6, 2, 0, False),     # H - east
-    WallRenderConfig(-20, 50, 8, 2, 0, False),    # I - east
+    WallRenderConfig(16, 66, 6, 2, 0, False),  # H - east
+    WallRenderConfig(-20, 50, 8, 2, 0, False),  # I - east
 
-    WallRenderConfig(-20, 58, 8, 2, 0, True),     # K - west
-    WallRenderConfig(16, 86, 6, 2, 0, True),      # L - west
+    WallRenderConfig(-20, 58, 8, 2, 0, True),  # K - west
+    WallRenderConfig(16, 86, 6, 2, 0, True),  # L - west
 
-    WallRenderConfig(62, 44, 8, 6, 4, False),     # I - south
-    WallRenderConfig(58, 50, 8, 10, 0, False),    # J - south
-    WallRenderConfig(58, 60, 8, 6, 4, False),     # K - south
+    WallRenderConfig(62, 44, 8, 6, 4, False),  # I - south
+    WallRenderConfig(58, 50, 8, 10, 0, False),  # J - south
+    WallRenderConfig(58, 60, 8, 6, 4, False),  # K - south
 
-    WallRenderConfig(-56, 25, 12, 3, 0, False),   # M - east
+    WallRenderConfig(-56, 25, 12, 3, 0, False),  # M - east
 
-    WallRenderConfig(-56, 38, 12, 3, 0, True),    # O - west
+    WallRenderConfig(-56, 38, 12, 3, 0, True),  # O - west
 
     WallRenderConfig(151, 22, 12, 3, 13, False),  # M - south
     WallRenderConfig(138, 41, 12, 3, 13, False),  # O - south
     WallRenderConfig(138, 25, 12, 16, 0, False),  # N - south
 
-    WallRenderConfig(-101, 0, 15, 3, 0, False),   # P - east
+    WallRenderConfig(-101, 0, 15, 3, 0, False),  # P - east
 
-    WallRenderConfig(-101, 19, 15, 3, 0, True)    # Q - west
+    WallRenderConfig(-101, 19, 15, 3, 0, True)  # Q - west
 ]
 
 
@@ -217,7 +238,7 @@ def draw_bg():
             sdl2.SDL_RenderCopyEx(renderer, bg_texture, src_rect, dst_rect, c_double(0.0), None, flag)
 
 
-def draw_walls(pos_x, pos_y, direction):
+def draw_walls(position, direction):
     """
     # @formatter:off
     
@@ -246,61 +267,95 @@ def draw_walls(pos_x, pos_y, direction):
             x = maze_pos_offsets[i].delta_x * direction.scale_x
             y = maze_pos_offsets[i].delta_y * direction.scale_y
 
-        wall_pos_x = x + pos_x
-        wall_pos_y = y + pos_y
+        wall_pos_x = x + position[0]
+        wall_pos_y = y + position[1]
 
         if wall_pos_y < 0 or wall_pos_x < 0 or wall_pos_y > 31 or wall_pos_x > 31:
             continue
 
         wall_direction = maze_pos_offsets[i].direction.from_view_dir(direction)
 
+        # if wall_pos_y == 0 and (wall_pos_x & 0x1) == 0x1:
+        #     wall_mapping_index = 0
+        # elif wall_pos_y == 0 or wall_pos_y == 2:
+        #     wall_mapping_index = 1
+        # else:
+        #     wall_mapping_index = 0
+
         wall_mapping_index = maze['walls'][wall_pos_x][wall_pos_y][wall_direction.symbol]
 
-        draw_wall(wall_mapping_index, i)
+        if wall_mapping_index == 0:
+            continue
+
+        draw_wmi(wall_mapping_index, i)
+
+
+def draw_wmi(wmi, wall_position):
+    def is_door_wmi(wmi):
+        return 2 < wmi < 23
+
+    def is_wall_wmi(wmi):
+        return wmi in [1, 2, 23, 24]
+
+    if is_wall_wmi(wmi):
+        draw_wall(wmi, wall_position)
+    elif is_door_wmi(wmi):
+        pass
+    else:
+        # decoration
+        pass
 
 
 def draw_wall(wall_mapping_index, wall_position):
-    # walls
     if wall_mapping_index == 1 or wall_mapping_index == 2:
-
         wall_type = wall_mapping_index - 1
+    else:
+        wall_type = wall_mapping_index - 20
 
-        cfg = walls_render_config[wall_position]
+    cfg = walls_render_config[wall_position]
 
-        tiles_offset = cfg.base_offset
+    tiles_offset = cfg.base_offset
 
-        tileset = vmp['wallTiles'][wall_type]['tiles']
+    tileset = vmp['wallTiles']
 
-        for t in range(cfg.blk_height):
+    for t in range(cfg.blk_height):
 
-            for s in range(cfg.blk_width):
+        for s in range(cfg.blk_width):
 
-                if cfg.flip:
-                    viewport_block_index = cfg.view_offset + cfg.blk_width - (s + 1) + t * VP_WIDTH_BLOCKS
-                else:
-                    viewport_block_index = s + t * VP_WIDTH_BLOCKS + cfg.view_offset
+            if cfg.flip:
+                viewport_block_index = cfg.view_offset + cfg.blk_width - (s + 1) + t * VP_WIDTH_BLOCKS
+            else:
+                viewport_block_index = s + t * VP_WIDTH_BLOCKS + cfg.view_offset
 
-                viewport_block_x = viewport_block_index % VP_WIDTH_BLOCKS
-                viewport_block_y = viewport_block_index // VP_WIDTH_BLOCKS
+            viewport_block_x = viewport_block_index % VP_WIDTH_BLOCKS
+            viewport_block_y = viewport_block_index // VP_WIDTH_BLOCKS
 
-                tile = tileset[tiles_offset]
-                flip = tile['flipped'] != cfg.flip  # xor
-                flag = sdl2.SDL_FLIP_HORIZONTAL if flip else sdl2.SDL_FLIP_NONE
+            tile = tileset[tiles_offset + 431*wall_type]
+            flip = tile['flipped'] != cfg.flip  # xor
+            flag = sdl2.SDL_FLIP_HORIZONTAL if flip else sdl2.SDL_FLIP_NONE
 
-                vcn_block = tile['vcn_block']
-                # vcn_block += 128
-                tileset_block_x = vcn_block % 32
-                tileset_block_y = vcn_block // 32
+            vcn_block = tile['vcn_block']
 
-                src_rect = sdl2.SDL_Rect(tileset_block_x * 8, tileset_block_y * 8, 8, 8)
-                dst_rect = sdl2.SDL_Rect(viewport_block_x * 8, viewport_block_y * 8, 8, 8)
+            tileset_block_x = vcn_block % 32
+            tileset_block_y = vcn_block // 32
 
-                sdl2.SDL_RenderCopyEx(renderer, walls_texture, src_rect, dst_rect, c_double(0.0),
-                                      None, flag)
+            src_rect = sdl2.SDL_Rect(tileset_block_x * 8, tileset_block_y * 8, 8, 8)
+            dst_rect = sdl2.SDL_Rect(viewport_block_x * 8, viewport_block_y * 8, 8, 8)
 
-                tiles_offset += 1
+            sdl2.SDL_RenderCopyEx(renderer, walls_texture, src_rect, dst_rect, c_double(0.0),
+                                  None, flag)
 
-            tiles_offset += cfg.skip
+            tiles_offset += 1
+
+        tiles_offset += cfg.skip
+
+
+KEY_W = 119
+KEY_S = 115
+KEY_A = 97
+KEY_D = 100
+KEY_Q = 113
+KEY_E = 101
 
 
 def run_raw():
@@ -330,6 +385,9 @@ def run_raw():
     bg_texture = get_vmp_bg_texture()
     walls_texture = get_vmp_wall_texture()
 
+    party_position = (5, 28)
+    party_direction = Direction.North
+
     running = True
     event = sdl2.SDL_Event()
     while running:
@@ -337,6 +395,21 @@ def run_raw():
             if event.type == sdl2.SDL_QUIT:
                 running = False
                 break
+            elif event.type == sdl2.SDL_KEYDOWN:
+                if event.key.keysym.sym == KEY_W:
+                    party_position = party_direction.update_pos(party_position)
+                elif event.key.keysym.sym == KEY_S:
+                    party_position = party_direction.opposite().update_pos(party_position)
+                elif event.key.keysym.sym == KEY_A:
+                    party_position = party_direction.previous_dir().update_pos(party_position)
+                elif event.key.keysym.sym == KEY_D:
+                    party_position = party_direction.next_dir().update_pos(party_position)
+                elif event.key.keysym.sym == KEY_Q:
+                    party_direction = party_direction.previous_dir()
+                elif event.key.keysym.sym == KEY_E:
+                    party_direction = party_direction.next_dir()
+
+                print(f'position: {party_position[0]}, {party_position[1]}, direction: {party_direction.symbol}')
 
         sdl2.SDL_SetRenderTarget(renderer, render_target)
         sdl2.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255)
@@ -344,7 +417,7 @@ def run_raw():
 
         draw_bg()
 
-        draw_walls(5, 7, Direction.North)
+        draw_walls(party_position, party_direction)
 
         sdl2.SDL_SetRenderTarget(renderer, None)
 
